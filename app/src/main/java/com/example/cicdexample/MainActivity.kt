57eca8f416fb412ac166b8058afdb95c95ef3e40
Ashutosh.Mishra <ashutosh.mishra@sportzinteractive.net>
2023-05-18 17:01:08 +0530

    AppCenter Added

--- app/src/main/java/com/example/cicdexample/MainActivity.kt
+++ app/src/main/java/com/example/cicdexample/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cicdexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "92b10c68-4eff-4c50-864c-9b2784f2d928",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
