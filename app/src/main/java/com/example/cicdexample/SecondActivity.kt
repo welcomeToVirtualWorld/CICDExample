856801f8ecea5edc3c16bb8dae9c82b7666d74b4
Ashutosh.Mishra <ashutosh.mishra@sportzinteractive.net>
2023-05-22 16:36:22 +0530

    CompileSDK 33

--- app/src/main/java/com/example/cicdexample/SecondActivity.kt
+++ app/src/main/java/com/example/cicdexample/SecondActivity.kt
@@ -7,5 +7,6 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
     }
 }
