591bee27791770ec0af941c41bbe8d4613303965
Ashutosh.Mishra <ashutosh.mishra@sportzinteractive.net>
2026-02-10 18:27:07 +0530

    forcefully build fail.

--- app/src/test/java/com/example/cicdexample/MathHelperTest.kt
+++ app/src/test/java/com/example/cicdexample/MathHelperTest.kt
@@ -8,6 +8,6 @@ class MathHelperTest {
     @Test
     fun `2 plus 2 should equal 4`() {
         val result = MathHelper.add(2, 2)
-        assertEquals(4, result)
+        assertEquals(3, result)
     }
 }
